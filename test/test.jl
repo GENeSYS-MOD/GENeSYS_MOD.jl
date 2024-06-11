c3a45525d3210c03c112631905d443fb7fd7cf5c
Zoe Blanchin <zoe.blanchin@sintef.no>
2024-06-11 09:07:53 +0200

    new demands for france, from renewables.ninja, 10 top cities

--- test/test.jl
+++ test/test.jl
@@ -6,10 +6,12 @@ using HiGHS
 using Ipopt
 using Gurobi
 
-model, data = genesysmod(;elmod_daystep = 80, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, 
+model, data = genesysmod(;elmod_daystep = 1, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, 
 inputdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Inputs"),
 resultdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Results"),
-data_file="RegularParameters_Europe_openENTRANCE_technoFriendly",
-hourly_data_file = "Timeseries_Europe_openENTRANCE_technoFriendly",
-switch_raw_results = 1
+data_file="RegularParameters_Europe_openENTRANCE_technoFriendly_FR",
+hourly_data_file = "Timeseries_Europe_openENTRANCE_technoFriendly_demand_Bordeaux",
+switch_raw_results = 1,
+write_reduced_timeserie = 0,
+data_base_region="FR"
 );
--- test/test_dispatch.jl
+++ test/test_dispatch.jl
@@ -9,10 +9,32 @@ using Gurobi
 model, data = genesysmod_simple_dispatch_two_nodes(;elmod_daystep = 0, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, 
 inputdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Inputs"),
 resultdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Results"),
-data_file="RegularParameters_Europe_openENTRANCE_technoFriendly_dispatch",
+data_file="RegularParameters_Europe_openENTRANCE_technoFriendly_dispatch_full_region",
 hourly_data_file = "Timeseries_Europe_openENTRANCE_technoFriendly",
-# switch_raw_results = 1,
+switch_raw_results = 1,
 # write_reduced_timeserie = 1,
 year=2050,
 # switch_processed_results =1
 );
+
+# model, data = genesysmod_simple_dispatch(;elmod_daystep = 0, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, 
+# inputdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Inputs"),
+# resultdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Results"),
+# data_file="RegularParameters_Europe_openENTRANCE_technoFriendly_dispatch",
+# hourly_data_file = "Timeseries_Europe_openENTRANCE_technoFriendly",
+# switch_raw_results = 1,
+# # write_reduced_timeserie = 1,
+# year=2050,
+# # switch_processed_results =1
+# );
+
+# model, data = genesysmod_simple_dispatch_one_node_storage(;elmod_daystep = 0, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, 
+# inputdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Inputs"),
+# resultdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Results"),
+# data_file="RegularParameters_Europe_openENTRANCE_technoFriendly_dispatch",
+# hourly_data_file = "Timeseries_Europe_openENTRANCE_technoFriendly",
+# switch_raw_results = 1,
+# # write_reduced_timeserie = 1,
+# year=2050,
+# # switch_processed_results =1
+# );
