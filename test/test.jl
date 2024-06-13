71551125c68d755636e39605ee6dffe67ab31b30
Morten Svendgård <morten.svendgard@sintef.no>
2024-06-13 15:16:18 +0200

    Made test.jl runnable by including necessary functions in GENeSYS_MOD.jl, while avoiding duplicate function definitions. Updated .gitignore

--- test/test.jl
+++ test/test.jl
@@ -1,14 +1,10 @@
-using Pkg
-Pkg.activate("GENeSYS_MOD")
-import GENeSYS_MOD
 using GENeSYS_MOD
-using HiGHS
 using Ipopt
 using Gurobi
 
 model, data = genesysmod(;elmod_daystep = 1, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, 
-inputdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Inputs"),
-resultdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Results"),
+inputdir = joinpath("test","TestData","Inputs"),
+resultdir = joinpath("test","TestData","Results"),
 data_file="RegularParameters_Europe_openENTRANCE_technoFriendly_FR",
 hourly_data_file = "Timeseries_Europe_openENTRANCE_technoFriendly_demand_Bordeaux",
 switch_raw_results = 1,
--- test/test_dispatch.jl
+++ test/test_dispatch.jl
@@ -1,14 +1,10 @@
-using Pkg
-Pkg.activate("GENeSYS_MOD")
-import GENeSYS_MOD
 using GENeSYS_MOD
-using HiGHS
 using Ipopt
 using Gurobi
 
-model, data = genesysmod_simple_dispatch_two_nodes(;elmod_daystep = 0, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, 
-inputdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Inputs"),
-resultdir = joinpath(pkgdir(GENeSYS_MOD),"test","TestData","Results"),
+model, data = genesysmod_simple_dispatch_two_nodes(;elmod_daystep = 0, elmod_hourstep = 1, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=4, 
+inputdir = joinpath("test","TestData","Inputs"),
+resultdir = joinpath("test","TestData","Results"),
 data_file="RegularParameters_Europe_openENTRANCE_technoFriendly_dispatch_full_region",
 hourly_data_file = "Timeseries_Europe_openENTRANCE_technoFriendly",
 switch_raw_results = 1,
