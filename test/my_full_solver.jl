439190e0ccb4eb1ad4485e681ba793f618bea8cd
Morten Svendgård <mortensvendgaard@gmail.com>
2024-07-31 15:55:49 +0200

    InvestLimit to 0, for making it run

--- test/my_full_solver.jl
+++ test/my_full_solver.jl
@@ -7,17 +7,17 @@ using Gurobi
 # (I also allow infesability tech for testing purposes)
 # Use Germany as the region to look to if there are missing data for a country
 
-model, data = genesysmod(;elmod_daystep = 40, elmod_hourstep = 4, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, threads=0, elmod_starthour = 0,
-    inputdir = joinpath("test","MyData","10_perc_storage"),
-    resultdir = joinpath("test","MyData","10_perc_storage"),
-    data_file= "Parameter_data_myscript_real",
-    hourly_data_file = "Timeseries_data_myscript_real",
+model, data = genesysmod(;elmod_daystep = 40, elmod_hourstep = 4, solver=Gurobi.Optimizer, DNLPsolver = Ipopt.Optimizer, elmod_starthour = 0,
+    inputdir = joinpath("test","TestData","DailyTest_NewCode"),
+    resultdir = joinpath("test","MyData","DailyTest_NewCode"),
+    data_file= "Parameter_data_myscript_test",
+    hourly_data_file = "Timeseries_data_myscript_test",
     switch_raw_results = 0, # To save results for use by dispatch code
     switch_infeasibility_tech = 0, # Allow expensive DummyTech if porblem is not feasable.
-    #switch_investLimit = 0,
+    switch_investLimit = 0,
     write_reduced_timeserie = 0,
     data_base_region="DE",
-    solution_file_name = "Solution_day_4_10_perc4.sol" #"Solution_40_12_day.sol", # "Solution_40_12_season.sol",
+    solution_file_name = "Solution_test.sol" #"Solution_40_12_day.sol", # "Solution_40_12_season.sol",
 );
 
 
