38d698fb1fb884bd8102f11c1aa18ff521828ac7
Dimitri Pinel <dimitri.pinel@sintef.no>
2025-08-12 15:28:38 +0200

    change Region to Region_full in scenariodata

--- src/genesysmod_scenariodata_europe.jl
+++ src/genesysmod_scenariodata_europe.jl
@@ -10,7 +10,7 @@ module ScenarioDataEurope
             Params.AvailabilityFactor[:,"X_DAC_LT",:] .= 0
         end
 
-        for r ∈ Sets.Region, t ∈ Sets.Technology, y ∈ Sets.Year
+        for r ∈ Sets.Region_full, t ∈ Sets.Technology, y ∈ Sets.Year
             if Params.TotalAnnualMaxCapacity[r,t,y] < Params.TotalAnnualMinCapacity[r,t,2025]
                 println("TotalAnnualMaxCapacity[$r,$t,$y] is lower than TotalAnnualMinCapacity[$r,$t,2025], check the data! Setting the value to TotalAnnualMinCapacity[$r,$t,2025].")
                 Params.TotalAnnualMaxCapacity[r,t,y] = Params.TotalAnnualMinCapacity[r,t,2025]
@@ -18,14 +18,14 @@ module ScenarioDataEurope
         end
 
         # Limit capacity expansion in 2025 to only actually (historically) installed capacities
-        for r ∈ Sets.Region, t ∈ setdiff(Params.Tags.TagTechnologyToSubsets["PowerSupply"],"P_Nuclear")
+        for r ∈ Sets.Region_full, t ∈ setdiff(Params.Tags.TagTechnologyToSubsets["PowerSupply"],"P_Nuclear")
             if  Params.TotalAnnualMinCapacity[r,t,2025] == 0
                 @constraint(model, Vars.NewCapacity[2025,t,r] <= Params.TotalAnnualMaxCapacity[r,t,2025], base_name="ScenarioData_Europe_NewCapacity_2025_$(t)_$(r)") #Shouldn't that be fixed to 0?
             end
         end
         #NewCapacity.up(2025,t,r)$(TagTechnologyToSubsets(t,"PowerSupply") and not TotalAnnualMinCapacity(r,t,2025) and not sameas(t,"P_Nuclear")) = TotalAnnualMinCapacity(r,t,2025);
 
-        for r ∈ Sets.Region, y ∈ Sets.Year
+        for r ∈ Sets.Region_full, y ∈ Sets.Year
             @constraint(model, Vars.ProductionByTechnologyAnnual[y,"CHP_WasteToEnergy","Heat_District",r] <= Params.RegionalBaseYearProduction[r,"CHP_WasteToEnergy","Heat_District",2018], base_name="ScenarioData_Europe_CHP_WasdteToEnergy_Heat_District_$(r)_$(y)")
             for f ∈ Sets.Fuel
                 Params.OutputActivityRatio[r,"CHP_WasteToEnergy",f,1,y] = 0
@@ -35,7 +35,7 @@ module ScenarioDataEurope
             end
         end
 
-        for r ∈ Sets.Region, y ∈ Sets.Year
+        for r ∈ Sets.Region_full, y ∈ Sets.Year
             tmp = copy(Params.SpecifiedAnnualDemand[r,"Heat_District",y])
             Params.SpecifiedAnnualDemand[r,"Heat_Buildings",y] = Params.SpecifiedAnnualDemand[r,"Heat_District",y]*0.85 + Params.SpecifiedAnnualDemand[r,"Heat_Buildings",y]
             Params.SpecifiedAnnualDemand[r,"Heat_District",y] = 0
@@ -47,7 +47,7 @@ module ScenarioDataEurope
         end
 
         if Switch.emissionPathway == "REPowerEU"
-            for r ∈ Sets.Region, y ∈ Sets.Year
+            for r ∈ Sets.Region_full, y ∈ Sets.Year
                 @constraint(model, Vars.ProductionByTechnologyAnnual[y,"HHI_Scrap_EAF","Heat_High_Industrial",r] <= Params.SpecifiedAnnualDemand[r,"Heat_High_Industrial",y]*0.65, base_name="ScenarioData_Europe_HHI_Scrap_EAF_Heat_High_Industrial_$(r)_$(y)")
             end
             for f ∈ Sets.Fuel
@@ -57,7 +57,7 @@ module ScenarioDataEurope
                 end
             end
         elseif Switch.emissionPathway == "NECPEssentials"
-            for r ∈ Sets.Region, y ∈ Sets.Year
+            for r ∈ Sets.Region_full, y ∈ Sets.Year
                 @constraint(model, Vars.ProductionByTechnologyAnnual[y,"HHI_Scrap_EAF","Heat_High_Industrial",r] <= Params.SpecifiedAnnualDemand[r,"Heat_High_Industrial",y]*0.6, base_name="ScenarioData_Europe_HHI_Scrap_EAF_Heat_High_Industrial_$(r)_$(y)")
             end
             for f ∈ Sets.Fuel
@@ -67,7 +67,7 @@ module ScenarioDataEurope
                 end
             end
         elseif Switch.emissionPathway == "Green"
-            for r ∈ Sets.Region, y ∈ Sets.Year
+            for r ∈ Sets.Region_full, y ∈ Sets.Year
                 @constraint(model, Vars.ProductionByTechnologyAnnual[y,"HHI_Scrap_EAF","Heat_High_Industrial",r] <= Params.SpecifiedAnnualDemand[r,"Heat_High_Industrial",y]*0.75, base_name="ScenarioData_Europe_HHI_Scrap_EAF_Heat_High_Industrial_$(r)_$(y)")
             end
             for f ∈ Sets.Fuel
@@ -77,7 +77,7 @@ module ScenarioDataEurope
                 end
             end
         elseif Switch.emissionPathway == "Trinity"
-            for r ∈ Sets.Region, y ∈ Sets.Year
+            for r ∈ Sets.Region_full, y ∈ Sets.Year
                 @constraint(model, Vars.ProductionByTechnologyAnnual[y,"HHI_Scrap_EAF","Heat_High_Industrial",r] <= Params.SpecifiedAnnualDemand[r,"Heat_High_Industrial",y]*0.5, base_name="ScenarioData_Europe_HHI_Scrap_EAF_Heat_High_Industrial_$(r)_$(y)")
             end
             for f ∈ Sets.Fuel
